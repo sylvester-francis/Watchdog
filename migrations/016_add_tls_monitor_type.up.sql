fea1ca1f3ba84012252ddd9e0a6a31e2c5ba1c02
Sylvester Francis <sylvesterranjithfrancis@gmail.com>
2026-02-17 20:36:53 -0500

    Add TLS to monitor type CHECK constraint
    Database CHECK constraint only allowed ping/http/tcp/dns, causing
    400 on TLS monitor creation despite domain and UI supporting it.

---
+++ migrations/016_add_tls_monitor_type.up.sql
@@ -0,0 +1,4 @@
+-- Add 'tls' to the allowed monitor types
+ALTER TABLE monitors DROP CONSTRAINT chk_monitor_type;
+ALTER TABLE monitors ADD CONSTRAINT chk_monitor_type
+    CHECK (type IN ('ping', 'http', 'tcp', 'dns', 'tls'));
